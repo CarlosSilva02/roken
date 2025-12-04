71e7389072fffbcb096935fdeb448b7aba852f45
carlos <silvacarlos081102@gmail.com>
2025-12-04 09:39:56 -0600

    fix: homepage not loading on GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CreateListing from "./pages/CreateListing";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
